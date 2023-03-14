7105e4613f7cdd55ed27c49b79c630e58beaf13f
cpr-fer <fer@cpr-robots.com>
2023-03-14 11:14:27 +0100

    Add more comments

--- irc_ros_controllers/src/dashboard_controller.cpp
+++ irc_ros_controllers/src/dashboard_controller.cpp
@@ -18,6 +18,10 @@ controller_interface::CallbackReturn DashboardController::on_init()
   return LifecycleNodeInterface::CallbackReturn::SUCCESS;
 }
 
+/**
+ * @brief Reads the configuration, starts publishers and services,
+ * creates semantic component interface.
+*/
 controller_interface::CallbackReturn DashboardController::on_configure(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
@@ -46,6 +50,9 @@ controller_interface::CallbackReturn DashboardController::on_configure(
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
+/**
+ * @brief Claim the dashboard command interfaces.
+ */
 controller_interface::InterfaceConfiguration DashboardController::command_interface_configuration()
   const
 {
@@ -63,6 +70,12 @@ controller_interface::InterfaceConfiguration DashboardController::command_interf
   return config;
 }
 
+/**
+ * @brief Sets the dashboard command interfaces.
+ * 
+ * While the SCI takes care of reading the values and creating the messages, it cannot configure
+ * the state interfaces itself.
+ */
 controller_interface::InterfaceConfiguration DashboardController::state_interface_configuration()
   const
 {
@@ -78,7 +91,9 @@ controller_interface::InterfaceConfiguration DashboardController::state_interfac
 
   return config;
 }
-
+/**
+ * @brief Assign state interfaces to SCIs.
+ */
 controller_interface::CallbackReturn DashboardController::on_activate(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
@@ -90,6 +105,9 @@ controller_interface::CallbackReturn DashboardController::on_activate(
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
+/**
+ * @brief Release the state interfaces from the SCIs.
+ */
 controller_interface::CallbackReturn DashboardController::on_deactivate(
   const rclcpp_lifecycle::State & /*previous_state*/)
 {
@@ -100,6 +118,9 @@ controller_interface::CallbackReturn DashboardController::on_deactivate(
   return controller_interface::CallbackReturn::SUCCESS;
 }
 
+/**
+ * @brief Only calls the publish method.
+ */
 controller_interface::return_type DashboardController::update(
   const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
 {
@@ -108,6 +129,9 @@ controller_interface::return_type DashboardController::update(
   return controller_interface::return_type::OK;
 }
 
+/**
+ * @brief Send the messages created by the SCIs for each module
+ */
 void DashboardController::publish()
 {
   auto module_states = irc_ros_msgs::msg::CanModuleStates();
@@ -121,28 +145,40 @@ void DashboardController::publish()
   }
 
   dashboard_publisher_->publish(module_states);
-
-  // state_interfaces_->get_name();
 }
 
+/**
+ * @brief Handle the dashboard command reception.
+ */
 void DashboardController::dashboard_command_callback(
   irc_ros_msgs::srv::CanModuleCommand_Request::SharedPtr req,
   irc_ros_msgs::srv::CanModuleCommand_Response::SharedPtr resp)
 {
   std::string name = req->name;
 
-  // TODO: Find position of req in joints_/gpios_
+  // Find position of req in command interfaces by the index in joints_/gpios_
+  // Since the command interfaces are filled first with joints and then gpios we get this messy
+  // search function
   int index;
+
+  // Search through joints first
   auto iter = std::find(joints_.begin(), joints_.end(), name);
   if (iter != joints_.end()) {
+    // Found, target is a joint at command_interfaces[index]
     index = iter - joints_.begin();
   } else {
+    // Not found, search through the gpios next
     iter = std::find(gpios_.begin(), gpios_.end(), name);
     index = iter - gpios_.begin();
     if (iter != gpios_.end()) {
+      // Found, target is a gpio at command_interfaces[index]
+
+      // Since the command interfaces are stored as
+      // [joint_1, ..., joint_n, gpio_1, ..., gpio_m]
+      // we need to add the number of joints as an offset
       index = joints_.size() + iter - gpios_.begin();
     } else {
-      // ERR: Not found
+      // Name not found
       resp->success = false;
       return;
     }
@@ -150,13 +186,13 @@ void DashboardController::dashboard_command_callback(
 
   command_interfaces_[index].set_value(req->type);
 
-  // Wait timeout milliseconds for ack, else respond with error
+  // Wait timeout_ milliseconds for ack, else respond with error
   std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
   int reply_value;
   while (std::chrono::steady_clock::now() - start < timeout_) {
     reply_value = static_cast<int>(command_interfaces_[index].get_value());
     if (reply_value == 0) {
-      // Success
+      // Success, the hardware interface acknowledged our message.
       break;
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
