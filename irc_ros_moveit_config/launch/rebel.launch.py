d546422738ea63c2a4ae7598028a79552120085a
Felix Reuter <fer@cpr-robots.com>
2023-04-14 13:41:57 +0200

    Started to make moveit files more generic

--- irc_ros_moveit_config/launch/rebel.launch.py
+++ irc_ros_moveit_config/launch/rebel.launch.py
@@ -1,6 +1,9 @@
 # Based on the panda moveit launch file from ros-planning
 # https://github.com/ros-planning/moveit_resources/blob/humble/panda_moveit_config/launch/demo.launch.py
 
+import os
+from ament_index_python.packages import get_package_share_directory
+from pathlib import Path
 from launch import LaunchDescription
 from launch.actions import (
     DeclareLaunchArgument,
@@ -9,18 +12,26 @@ from launch.actions import (
 )
 from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import (
+    Command,
+    FindExecutable,
     LaunchConfiguration,
     PathJoinSubstitution,
 )
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
-from moveit_configs_utils import MoveItConfigsBuilder
+from launch_param_builder import load_yaml
+from nav2_common.launch import ReplaceString
 
 
 def generate_launch_description():
+    namespace_arg = DeclareLaunchArgument("namespace", default_value="")
+    prefix_arg = DeclareLaunchArgument("prefix", default_value="")
+    controller_manager_name_arg = DeclareLaunchArgument(
+        "controller_manager_name",
+        default_value=[LaunchConfiguration("namespace"), "/controller_manager"],
+    )
     use_rviz_arg = DeclareLaunchArgument(
         "use_rviz",
         default_value="true",
@@ -51,6 +62,9 @@ def generate_launch_description():
     )
     use_rviz = LaunchConfiguration("use_rviz")
     # gripper = LaunchConfiguration("gripper")
+    namespace = LaunchConfiguration("namespace")
+    prefix = LaunchConfiguration("prefix")
+    controller_manager_name = LaunchConfiguration("controller_manager_name")
 
     rviz_file = PathJoinSubstitution(
         [FindPackageShare("irc_ros_moveit_config"), "rviz", "moveit.rviz"]
@@ -63,19 +77,94 @@ def generate_launch_description():
             "ros2_controllers.yaml",
         ]
     )
-
-    # TODO: How to set urdf/xacro parameters?
-    moveit_config = (
-        MoveItConfigsBuilder("irc_ros")
-        .robot_description(file_path="config/igus_rebel_6dof.urdf.xacro")
-        .robot_description_semantic(file_path="config/igus_rebel_6dof.srdf")
-        .trajectory_execution(file_path="config/moveit_controllers.yaml")
-        .planning_pipelines(
-            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]
-        )
-        .robot_description_kinematics(file_path="config/kinematics.yaml")
-        .to_moveit_configs()
+    ros2_controllers = ReplaceString(
+        source_file=ros2_controllers_file,
+        replacements={
+            "<namespace>": namespace,
+            "<prefix>": prefix,
+        },
     )
+
+    robot_description_file = PathJoinSubstitution(
+        [
+            FindPackageShare("irc_ros_description"),
+            "urdf",
+            "igus_rebel_6dof.urdf.xacro",
+        ]
+    )
+    robot_description = Command(
+        [
+            FindExecutable(name="xacro"),
+            " ",
+            robot_description_file,
+            " prefix:=",
+            prefix,
+            " hardware_protocol:=",
+            "cprcanv2",
+        ]
+    )
+    robot_description_semantic_file = PathJoinSubstitution(
+        [
+            FindPackageShare("irc_ros_moveit_config"),
+            "config",
+            "igus_rebel_6dof.srdf.xacro",
+        ]
+    )
+    robot_description_semantic = Command(
+        [
+            FindExecutable(name="xacro"),
+            " ",
+            robot_description_semantic_file,
+            " prefix:=",
+            prefix,
+        ]
+    )
+
+    # Requires the os.path.join way instead of PathJoinSubstitution here
+    controllers_file = os.path.join(
+        get_package_share_directory("irc_ros_moveit_config"),
+        "config",
+        "controllers.yaml",
+    )
+    # TODO: This needs to return a path or the line below it wont accept it
+    # Until then the namespace/prefix replacements won't work
+    # controllers = ReplaceString(
+    #     source_file=controllers_file,
+    #     replacements={
+    #         "<namespace>": namespace,
+    #         "<prefix>": prefix,
+    #     }
+    # )
+    controllers_dict = load_yaml(Path(controllers_file))
+
+    moveit_controllers = {
+        "moveit_simple_controller_manager": controllers_dict,
+        "moveit_controller_manager": "moveit_simple_controller_manager/MoveItSimpleControllerManager",
+    }
+
+    robot_description_kinematics_file = PathJoinSubstitution(
+        [
+            FindPackageShare("irc_ros_moveit_config"),
+            "config",
+            "kinematics.yaml",
+        ]
+    )
+    robot_description_kinematics = ReplaceString(
+        source_file=robot_description_kinematics_file,
+        replacements={
+            "<namespace>": namespace,
+            "<prefix>": prefix,
+        },
+    )
+
+    # TODO: Stolen from UR ROS2 for testing purposes:
+    planning_pipeline = {
+        "move_group": {
+            "planning_plugin": "ompl_interface/OMPLPlanner",
+            "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
+            "start_state_max_bounds_error": 0.1,
+        }
+    }
 
     planning_scene_monitor_parameters = {
         "publish_planning_scene": True,
@@ -88,7 +177,11 @@ def generate_launch_description():
         package="moveit_ros_move_group",
         executable="move_group",
         parameters=[
-            moveit_config.to_dict(),
+            {"robot_description": robot_description},
+            {"robot_description_semantic": robot_description_semantic},
+            robot_description_kinematics,
+            planning_pipeline,
+            moveit_controllers,
             {"publish_robot_description_semantic": True},
             planning_scene_monitor_parameters,
         ],
@@ -98,8 +191,8 @@ def generate_launch_description():
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[
-            moveit_config.robot_description,
-            ros2_controllers_file,
+            {"robot_description": robot_description},
+            ros2_controllers,
         ],
     )
 
@@ -108,13 +201,15 @@ def generate_launch_description():
         executable="robot_state_publisher",
         name="robot_state_publisher",
         # namespace=namespace,
-        parameters=[moveit_config.robot_description],
+        parameters=[
+            {"robot_description": robot_description},
+        ],
     )
 
     joint_state_broadcaster_node = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster", "-c", "/controller_manager"],
+        arguments=["joint_state_broadcaster", "-c", controller_manager_name],
     )
 
     rebel_6dof_controller_node = Node(
@@ -154,15 +249,17 @@ def generate_launch_description():
         name="rviz2",
         arguments=["-d", rviz_file],
         parameters=[
-            moveit_config.robot_description,
-            moveit_config.robot_description_semantic,
-            moveit_config.planning_pipelines,
-            moveit_config.robot_description_kinematics,
+            {"robot_description": robot_description},
+            {"robot_description_semantic": robot_description_semantic},
+            planning_pipeline,
+            robot_description_kinematics,
         ],
         condition=IfCondition(use_rviz),
     )
     ld = LaunchDescription()
-
+    ld.add_action(namespace_arg)
+    ld.add_action(prefix_arg)
+    ld.add_action(controller_manager_name_arg)
     ld.add_action(use_rviz_arg)
     ld.add_action(gripper_arg)
     ld.add_action(launch_dashboard_controller_arg)
